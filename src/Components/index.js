6dbb7205271f3207f094ef9e905fb2f23abce1fc
JakeAdmire <jakeadmire1@gmail.com>
2020-03-25 13:55:30 -0600

    [enhancement]: Add all docs components to master export file

---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+export { BrandHighlight } from './BrandHighlight/BrandHighlight';
+export { Subtitle } from './Subtitle/Subtitle';
+export { Subheading } from './Subheading/Subheading';
+export { ComponentWrapper } from './ComponentWrapper/ComponentWrapper';
+export { Bookmark } from './Bookmark/Bookmark';
+export { Spacer } from './Spacer/Spacer';
