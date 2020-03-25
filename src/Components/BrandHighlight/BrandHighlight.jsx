4ae46f780608b3ebf0bc7674f6a9dfd335bd8a0f
JakeAdmire <jakeadmire1@gmail.com>
2020-03-24 18:01:56 -0600

    Fix formatting

--- src/Components/BrandHighlight/BrandHighlight.jsx
+++ src/Components/BrandHighlight/BrandHighlight.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const BrandHighlight = ({ override }) => (
-<span style={{ fontWeight: 500, color: '#0097e1' }}>{override || 'blueprints-ui'}</span>
+    <span style={{ fontWeight: 500, color: '#0097e1' }}>{override || 'blueprints-ui'}</span>
 );
