dc45ce3ee305785ea6914f8cf5d4f435dcca0b54
JakeAdmire <jakeadmire1@gmail.com>
2020-04-10 18:09:27 -0600

    Add MarkdownModal to master exports

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,3 +4,4 @@ export { Subheading } from './Subheading/Subheading';
 export { ComponentWrapper } from './ComponentWrapper/ComponentWrapper';
 export { Bookmark } from './Bookmark/Bookmark';
 export { Spacer } from './Spacer/Spacer';
+export { MarkdownModal } from './MarkdownModal/MarkdownModal';
